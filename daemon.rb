9e73a784247c7d82a1493475d1061ec58e2f3d38
Alex Slynko <alex.slynko@wonga.com>
2013-08-09 14:49:41 +0100

    TD-1041: Convert to new daemon format

--- daemon.rb
+++ daemon.rb
@@ -1,35 +1,12 @@
 #!/usr/bin/env ruby
-
 require 'rubygems'
-require 'daemons'
-require 'common/publisher'
-require 'common/subscriber'
-require 'common/config'
+require 'wonga/daemon'
 require_relative 'ec2_bootstrapped_event_handler/ec2_bootstrapped_event_handler'
 
-THIS_FILE = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
-config = Daemons::Config.new(
-  File.expand_path(
-    File.join(File.dirname(THIS_FILE),"config", "daemon.yml")
-  )
-)
+config_name = File.join(File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__), "config", "daemon.yml")
+Wonga::Daemon.load_config(File.expand_path(config_name))
 
-daemon_config = {
-  :backtrace => config['daemon']['backtrace'],
-  :dir_mode => config['daemon']['dir_mode'].to_sym,
-  :dir => "#{File.expand_path(config['daemon']['dir'])}",
-  :monitor => config['daemon']['monitor']
-}
-
-Daemons.run_proc(config['daemon']['app_name'], daemon_config) {
-  begin
-    Daemons::Subscriber.new.subscribe(
-      config['sqs']['queue_name'],
-      Daemons::EC2BootstrappedEventHandler.new(config)
-    )
-  rescue => e
-    puts "#{e}"
-    retry
-  end
-}
+config = Wonga::Daemon.config
+api_client = Wonga::Daemon::PantryApiClient.new(config['pantry']['url'], config['pantry']['api_key'], Wonga::Daemon.logger, config['pantry']['timeout'])
+Wonga::Daemon.run(Wonga::Daemon::EC2BootstrappedEventHandler.new(api_client, Wonga::Daemon.logger))
 
--- spec/ec2_bootstrapped_event_handler/ec2_bootstrapped_event_handler_spec.rb
+++ spec/ec2_bootstrapped_event_handler/ec2_bootstrapped_event_handler_spec.rb
@@ -1,33 +1,14 @@
-require_relative "../../ec2_bootstrapped_event_handler/ec2_bootstrapped_event_handler"
 require 'spec_helper'
+require_relative "../../ec2_bootstrapped_event_handler/ec2_bootstrapped_event_handler"
 
-describe Daemons::EC2BootstrappedEventHandler do
-  subject { Daemons::EC2BootstrappedEventHandler.new() }
+describe Wonga::Daemon::EC2BootstrappedEventHandler do
+  let(:api_client) { instance_double('Wonga::Daemon::PantryApiClient').as_null_object }
+  subject { Wonga::Daemon::EC2BootstrappedEventHandler.new(api_client, instance_double('Logger').as_null_object) }
+  let(:message) { { 'pantry_request_id' => 42 } }
+  it_behaves_like "handler"
 
-  let(:good_message_hash) {
-    {
-      "Message"=>{
-        "request_id"=>1,
-        "instance_id"=>"i-f4819cb9"
-      }
-    }
-  }
-  let(:good_message_json) { good_message_hash.to_json }
-  let(:good_payload_json) { good_message_json["Message"].to_json }
-  let(:url) { "some_url" }
-  let(:api_key) { "some_api_key"}
-  let(:timeout) { 10 }
-
-  describe "#update_instance_booted_status" do
-    it "Updates the instance boot status from a good message" do
-      expect{subject.update_instance_booted_status(url,good_payload_json,api_key,timeout)}.not_to be false
-    end
+  it "updates Pantry using PantryApiClient" do
+    expect(api_client).to receive(:update_ec2_instance).with(42, { bootstrapped: true })
+    subject.handle_message(message)
   end
-
-  describe "#handle_message" do
-    it "Receives a correct SQS message and proceeds" do
-      expect{subject.handle_message(good_message_json)}.not_to be false
-    end
-  end
-
 end
